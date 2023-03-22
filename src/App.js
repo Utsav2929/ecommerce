818f5ff1a62c357843eb9a59b047e8e7a57bb0af
UTSAV MANDHANI <85448958+Utsav2929@users.noreply.github.com>
2023-03-22 12:26:26 +0530

    ecommerce 1 commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from 'react'
+import {commerce} from './lib/commerce'
 
-function App() {
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Products from './components/Products/Products';
+import Navbar from './components/Navbar/Navbar';
+import Cart from './components/Cart/Cart';
+import Checkout from './components/CheckoutForm/Checkout/Checkout';
+
+const App = () => {
+    const [products,setProducts]=useState([]);
+    const [cart,setCart] =useState({});
+
+    const fetchProducts=async()=>{
+        const {data} =await commerce.products.list();
+        setProducts(data);
+
+    };
+    const fetchCart=async()=>{
+        
+        setCart(await commerce.cart.retrieve());
+    }
+    const handleAddToCart= async( productId,quantity)=>{
+const response=await commerce.cart.add(productId,quantity);
+
+
+setCart(response.cart);
+console.log("addded",cart);
+    }
+
+
+// console.log(cart);
+
+
+const handleUpdateCartQty= async(productId,quantity)=>{
+    const response=await commerce.cart.update(productId,{quantity});
+    setCart(response.cart);
+    
+console.log("addded1",cart);
+}
+
+
+const handleRemoveFromCart= async (productId)=>{
+    const response=await commerce.cart.remove(productId);
+    setCart(response.cart);
+    
+console.log("addded2",cart);
+
+}
+const handleEmptyCart=async()=>{
+    const response= await commerce.cart.empty();
+    setCart(response.cart);
+    
+console.log("addded3",cart);
+}
+
+
+    
+useEffect(()=>{
+    fetchProducts();
+    fetchCart();
+    },[cart]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    <Router>
+    <div>    <Navbar totalItems={cart?.total_items}/>
+
+    <Switch>
+<Route exact path ="/">
+
+<Products  products={products} onAddToCart={handleAddToCart}/>
+</Route>
+
+<Route exact path ="/cart">
+    
+<Cart cart={cart} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart} onEmptyCart={handleEmptyCart} />
+         
+</Route>
+<Route path="/checkout" exact>
+            <Checkout />
+          </Route>
+    </Switch>
+        </div>
+    </Router>
+  )
 }
 
 export default App;
